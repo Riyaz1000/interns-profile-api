de08c5d82f524e9626eb87359ab6e2d64103414c
Riyaz <91373561+Riyaz1000@users.noreply.github.com>
2024-01-10 15:16:30 +0530

    fix: UI

--- assets/js/main.js
+++ assets/js/main.js
@@ -13,18 +13,10 @@ internId.addEventListener("click", (e) => {
 
   document.getElementById("result").innerHTML = "";
 
-  //   fetch("https://api.github.com/users/" + spaceRemove)
-  //     .then((result) => result.json())
-  //     .then((data) => {
-  //       console.log(data);
-  //       document.getElementById("result").innerHTML = `
-  //     <a target="_blank" href="https://www.github.com/${spaceRemove}">
-  //       <img src="${data.avatar_url}" alt="${spaceRemove}'s avatar"/>
-  //     </a>
-  //   `;
-  //     });
-
-  fetch("https://api.github.com/users/" + spaceRemove)
+  fetch(
+    "https://cors-anywhere.herokuapp.com/https://api.github.com/users/" +
+      spaceRemove
+  )
     //https://api.github.com/users/Riyaz1000/repos
     .then((result) => result.json())
     .then((data) => {
@@ -35,3 +27,45 @@ internId.addEventListener("click", (e) => {
       `;
     });
 });
+
+const allProfile = document.querySelector("#profile");
+const xhr = new XMLHttpRequest();
+
+const userName = [
+  "esakki2104prsnl",
+  "vk2401",
+  "suriyamassmsd",
+  "riyaz1000",
+  "hema-venkat3",
+  "yrd369",
+  "mushkir",
+  "bearcin46",
+  "dineshdevelope",
+  "jeya-rosini",
+  "swethadsalvatore",
+  "mshajid",
+  "muthuakalya",
+  "vedhatech002",
+  "muthukumarimoorthi",
+  "sharif-22",
+  "danielace1",
+  "gayathrihg",
+  "kishorekumar-kp",
+];
+
+// console.log(userName);
+
+xhr.onreadystatechange = function () {
+  //   console.log(this);
+
+  if (this.readyState === 4 && this.status === 200) {
+    // console.log(xhr.responseText);
+    allProfile.forEach((userName) => {
+      const repoLink = `https://api.github.com/users/${userName}/repos`;
+      //   console.log(repoLink);
+    });
+  }
+};
+
+xhr.open("GET", "https://api.github.com/users/<USER-NAME>/repos");
+xhr.send();
